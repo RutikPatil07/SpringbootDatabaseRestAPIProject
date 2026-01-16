7cabde262e8a2866db08645c59e5ae92d9df452a
Mr. Rutik Patil <patilrutik500@gmail.com>
2026-01-16 23:02:42 +0530

    Create Passbookrepo.java

---
+++ Passbookrepo.java
@@ -0,0 +1,30 @@
+package com.cs.demo.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.cs.demo.Entity.Rdpassbook;
+import com.cs.demo.pdto.UserPassbookDTO;
+
+import java.util.List;
+
+public interface Passbookrepo extends JpaRepository<Rdpassbook, Integer> {
+ @Query(value = "SELECT SUM(rdamt) FROM rdpassbook", nativeQuery = true)
+		 Long getTotalAmt();
+ 
+ 
+ @Query(value = "SELECT * FROM rdpassbook WHERE rid = :rid ", nativeQuery = true)
+ List<Rdpassbook> getAllByRid(@Param("rid") int rid);
+ 
+ @Query(value ="select name,acno,rdpassbook.rdamt,rdpassbook.rddate from rduser inner join rdpassbook on rduser.rid =\r\n"
+ 		+ "rdpassbook.rid", nativeQuery = true)
+ List<Object[]> getUserPassbookDetails();
+ 
+ @Query(value ="select name,acno,rdpassbook.rdamt,rdpassbook.rddate from rduser inner join rdpassbook on rduser.rid =\r\n"
+	 		+ "rdpassbook.rid", nativeQuery = true)
+	 List<UserPassbookDTO> getUserPassbookDetail();
+	}
+
+
+
