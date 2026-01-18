fa2c3784a1b724d49b679c864889f462751e5cc0
Mr. Rutik Patil <patilrutik500@gmail.com>
2026-01-18 15:12:14 +0530

    Update Passbookrepo.java

--- Passbookrepo.java
+++ Passbookrepo.java
@@ -24,6 +24,14 @@ public interface Passbookrepo extends JpaRepository<Rdpassbook, Integer> {
  @Query(value ="select name,acno,rdpassbook.rdamt,rdpassbook.rddate from rduser inner join rdpassbook on rduser.rid =\r\n"
 	 		+ "rdpassbook.rid", nativeQuery = true)
 	 List<UserPassbookDTO> getUserPassbookDetail();
+
+	@Query(value = "select name,acno,rdpassbook.rdamt,rdpassbook.rddate from rduser inner join rdpassbook on rduser.rid =\r\n"
+		 + "rdpassbook.rid WHERE rdpassbook.rid = :rid", nativeQuery = true)
+ List<UserPassbookDTO> getUserPassbookDetailById(@Param("rid") int rid);
+ 
+ @Query(value = "select name,acno,rdpassbook.rdamt,rdpassbook.rddate from rduser inner join rdpassbook on rduser.rid =\r\n"
+		 + "rdpassbook.rid WHERE rdpassbook.rid = :rid", nativeQuery = true)
+ List<UserPassbookDTO> getUserPassbookDetailBId(@Param("rid") int rid);
 	}
 
 
